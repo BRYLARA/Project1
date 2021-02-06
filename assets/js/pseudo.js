ae38bd9361155fc564d86a568fb4a8e91a808f18
b00kbaker <caimmurphy@gmail.com>
2021-02-06 11:50:20 -0700

    Starting pseudo code

---
+++ assets/js/pseudo.js
@@ -0,0 +1,12 @@
+// Global var 
+
+
+// What we want to happen:
+  //Search maps  (parameters: miles from input location, name of park)
+  //NP open/close status & link Activities?    
+  //Weather (parameter: current day, temperature, basic status (rain/snow/sun))   
+
+
+
+// Functions
+  //checklocalStorage at the beginning every time   
