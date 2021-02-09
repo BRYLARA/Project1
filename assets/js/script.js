d4f1b51958db9818e1d731cd692c1ba3e539d5d6
Drissa Bagate <lemanou7@yahoo.fr>
2021-02-08 21:14:59 -0700

    added api fetch

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+var api_key = 'vGDXf8DoFmcbZXhc3BjABck16B2RdO6qNrXKXX1E'
+var currentState = ''
+// GETTING ALL PARKS USING STATE CODE  
+
+
+var getApi = function(){
+    let stateCode = $('#state-input').val();
+    console.log(stateCode);
+    let requestUrl = 'https://developer.nps.gov/api/v1/parks?stateCode='+ stateCode +'&api_key='+ api_key;
+    fetch(requestUrl)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+    console.log(data);
+    for (let i=0; i< data.data.length; i++){
+
+        var parkEl =document.getElementById("showPark");
+        // var directionEl =document.getElementById("showPark");
+        // var basicInfoskEl =document.getElementById("showParkBasicInfos");
+        // var activitiesEl =document.getElementById("showParkDirection");
+
+        let p1El = document.createElement("p");
+        // let p2El = document.createElement("p");
+        // let p3El = document.createElement("p");
+        
+
+        p1El.textContent = data.data[i].fullName ;
+        // p2El.textContent =  data.data[i].description;
+        // p3El.textContent = data.data[i].directionsInfo;
+
+        parkEl.appendChild(p1El);
+        // basicInfoskEl.appendChild(p2El);
+        // directionEl.appendChild(p3El);
+        
+
+    }
+    });
+};
+
+
+
+// New city search button event listener
+$('#submit-button').on("click", (event) => {
+    event.preventDefault();
+    currentState = $('#state-input').val();
+    console.log(event);
+    getApi();
+});
+
+
+var getLocation = function(latitude,longitude){
+//  1-get google api 
+//  2-get user latidude and longitude
+//  3-compare all park locations to the user's and display the closest  
+};
