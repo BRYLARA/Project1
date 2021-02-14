341c6ae39b315375660306da3704b52f114ff402
b00kbaker <caimmurphy@gmail.com>
2021-02-14 16:37:40 -0700

    Set storage is working but not get storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 var api_key = "vGDXf8DoFmcbZXhc3BjABck16B2RdO6qNrXKXX1E";
 
-// Caitlin State stuff
+
+// Display current State parks categories
 
 function statePark(Statedata) {
+  
   var emptyStates = [];
 
   for (var i = 0; i < Statedata.data.length; i++) {
@@ -25,9 +27,9 @@ function statePark(Statedata) {
     listParks.addEventListener("click", function (e) {
       var index = e.target.getAttribute("data-idx");
       console.log(index);
-      var parkInfo = Statedata.data[index].description;
-      var seeInfo = document.getElementById("park-info");
-      seeInfo.append(parkInfo);
+      var seeInfo = Statedata.data[index].description;
+      var postInfo = document.getElementById("park-info");
+      postInfo.append(seeInfo);
 
       var seeWeather = Statedata.data[index].weatherInfo;
       var postWeather = document.getElementById("park-weather-info");
@@ -36,6 +38,9 @@ function statePark(Statedata) {
       var seeImg = Statedata.data[index].images[0].url;
 
       var postImg = (document.getElementById("park-image").src = seeImg);
+
+      localStorage.setItem(("data-idx",index), seeInfo);
+      
 
       var activitiesArray = [];
       for (var i = 0; i < 20; i++) {
@@ -48,12 +53,16 @@ function statePark(Statedata) {
         var parks = document.getElementById("park-activities");
         parks.append(list);
       });
+      
+      var viewLast= localStorage.getItem(("data-idx", index));
+      document.getElementById("park-info").innerHTML = viewLast;
+
     });
   });
   return;
 }
 
-// Caitlin and Drissa
+// Nested fetch calls to get State
 requestOptions = {
   method: "GET",
   redirect: "follow",
@@ -85,10 +94,8 @@ fetch("https://ipapi.co/json/", requestOptions)
         if (Statedata) {
           statePark(Statedata);
         }
-    
+
         return;
       });
   });
 
-// LocalStorage function
-// Click event on "Random Park" button, outputs from name array[i] and saves to local storage (Set localStorage). When page loads (get localStorage) and display last result from pushing "Random Park". If button is pushed again recognize two items in localStorage and only save 1 so not too much info is displayed. Maybe write a message to user before button is clicked? "Discover other parks in the US!"?
